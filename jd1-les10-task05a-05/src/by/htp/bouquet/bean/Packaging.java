5753c640d80874eac2b5e42a91906838ab4ee9b3
Misha-Prudnikov-dev <54108720+Misha-Prudnikov-dev@users.noreply.github.com>
2020-02-07 19:57:33 +0300

    Add files via upload

---
+++ jd1-les10-task05a-05/src/by/htp/bouquet/bean/Packaging.java
@@ -0,0 +1,66 @@
+package by.htp.bouquet.bean;
+
+public class Packaging {
+
+	private int price;
+	private String title;
+
+	public Packaging() {
+
+	}
+
+	public Packaging(String title, int price) {
+		this.title = title;
+		this.price = price;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + price;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Packaging other = (Packaging) obj;
+		if (price != other.price)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Packaging [price=" + price + ", title=" + title + "]";
+	}
+
+}
